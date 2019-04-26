6ada227ba3a1b329f1875c9b353941f20a647ef4
JCGrant <jamescolin.grant@gmail.com>
2019-04-26 21:55:20 +0100

    Add dropping items

--- src/main.rs
+++ src/main.rs
@@ -481,6 +481,22 @@ fn use_item(
     }
 }
 
+fn drop_item(
+    inventory_id: usize,
+    inventory: &mut Vec<Object>,
+    objects: &mut Vec<Object>,
+    messages: &mut Messages,
+) {
+    let mut item = inventory.remove(inventory_id);
+    item.set_pos(objects[PLAYER].x, objects[PLAYER].y);
+    message(
+        messages,
+        format!("You dropped a {}.", item.name),
+        colors::YELLOW,
+    );
+    objects.push(item);
+}
+
 /// return the position of a tile left-clicked in player's FOV (optionally in a
 /// range), or (None,None) if right-clicked.
 fn target_tile(
@@ -1226,6 +1242,19 @@ fn handle_keys(
             );
             if let Some(inventory_index) = inventory_index {
                 use_item(inventory_index, inventory, objects, messages, map, tcod);
+            }
+            DidntTakeTurn
+        }
+
+        (Key { printable: 'd', .. }, true) => {
+            // show the inventory; if an item is selected, drop it
+            let inventory_index = inventory_menu(
+                inventory,
+                "Press the key next to an item to drop it, or any other to cancel.\n'",
+                &mut tcod.root,
+            );
+            if let Some(inventory_index) = inventory_index {
+                drop_item(inventory_index, inventory, objects, messages);
             }
             DidntTakeTurn
         }
